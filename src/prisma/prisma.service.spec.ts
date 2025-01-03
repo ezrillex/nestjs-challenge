53b94b853f86f0dbcae7ade5dcf448f3712ceede
Ezra Abarca <39611157+ezrillex@users.noreply.github.com>
2025-01-03 10:21:20 -0600

    tests/ categories service, prisma service definition test

---
+++ src/prisma/prisma.service.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should have init method defined', () => {
+    expect(service.onModuleInit).toBeDefined();
+  });
+
+  it('should inherit prisma client', async () => {
+    expect(service).toBeInstanceOf(PrismaClient);
+  });
+});
